ce07b5afa62598d8a3e406ad4295418f46ef7a70
akash <akashes5753279@gmail.com>
2025-03-30 03:19:31 +0530

    separate iniitalstates and reducers to avoid complexity going forward , use combinereducers function to combine reducers because store accepts only one reducer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 import { createStore } from "redux";
+import { combineReducers } from "redux";
 
 const ORDER_PIZZA="ORDER_PIZZA"; // to avoid typos when using multiple times
 const ORDER_BURGER= 'ORDER_BURGER'; 
@@ -25,20 +26,35 @@ function orderBurger(){
     
 }
 
-const initialState={
+const initialStateForPizza={
     pizzaBase:100,
+    
+}
+const initialStateForBurger={
     burgerBuns:50,
+
     
 }
 //Reducer
 
-const reducer=(state=initialState,action)=>{
+const reducerPizza=(state=initialStateForPizza,action)=>{
     switch(action.type){
         case 'ORDER_PIZZA' :
             return {...state,pizzaBase:state.pizzaBase-1}
 
-            case "ORDER_BURGER":
-             return { ...state,burgerBuns:state.burgerBuns-1  }
+           
+
+            default:
+                return state
+
+    }
+
+}
+const reducerBurger=(state=initialStateForBurger,action)=>{
+    switch(action.type){
+        case "ORDER_BURGER":
+            return { ...state,burgerBuns:state.burgerBuns-1  }
+           
 
             default:
                 return state
@@ -50,8 +66,12 @@ const reducer=(state=initialState,action)=>{
 
 //store 
 // getState() // returns the current state of the store
-
-const store = createStore(reducer); // create a store with the reducer
+//combiner reducers before passing to store , because store only accepts one reducer
+const rootReducer = combineReducers({
+    pizza:reducerPizza,
+    burger:reducerBurger
+})
+const store = createStore(rootReducer); // create a store with the reducer
 //?instead of passing initialState why we are passing reducer :because reducer contains the  initial state and also the logic for state changes
 
 console.log('initial state',store.getState()); // get the initial state of the store
